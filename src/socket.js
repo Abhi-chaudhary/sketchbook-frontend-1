86bfb57febdb27abc743a14db5852eab3f9adab2
Abhi-chaudhary <chaudharyg15112001@gmail.com>
2024-02-26 22:40:03 +0530

    changed

--- src/socket.js
+++ src/socket.js
@@ -1,2 +1,3 @@
 import { io } from "socket.io-client";
-export const socket = io('http://localhost:5000');
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-eirc.onrender.com' : 'http://localhost:5000'
+export const socket = io('https://sketchbook-server-eirc.onrender.com');
